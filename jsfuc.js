6bbfff60297209cb74e6f86e426e56156d4d9e5c
Optfan <cpufan2001@outlook.com>
2023-03-16 19:37:32 +0800

    with git

--- jsfuc.js
+++ jsfuc.js
@@ -16,9 +16,10 @@ function QQopenINbrowser() {
     ope.click();
 
 }
-function browserTypeAlert()
-{
-    
+
+
+function browserTypeAlert() {
+
     if (deviceType) {
         let container = document.querySelector('.container')
         let headalert = document.createElement("div");
@@ -43,10 +44,12 @@ function ListenPICshow() {
                         console.log("loaded " + src);
                         wrap.outerHTML = `<img src='${base64}' class='show' data-src='${src}'>`;
                         let button = document.querySelector(`#illustcontainer>.col>div>img[data-src="${src}"]+div>button`)
+
                         if (!deviceType) {
                             button.disabled = false
                             button.className = 'btn btn-outline-primary float-end'
                         }
+
                     });
                     io.unobserve(wrap);
 
@@ -58,49 +61,36 @@ function ListenPICshow() {
         document.querySelectorAll('img.lazy[data-src]').forEach(function (img) {
             let src = img.dataset.src;
             img.outerHTML = `<div class='lazyImg-wrap' data-src='${src}'>
+            
             <div class="placeholder-glow" style="height:360px;width:100%">
                 <span>
                     <span class="placeholder col-12" style="height:100%;width:100%"></span>
 
                 </span>
-              </div>
-        </div>`;
+                <div class="progress">
+                    <div class="pixiv-green progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example"  aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
+                    </div>
+                </div>
+            </div>`;
         });
         document.querySelectorAll('.lazyImg-wrap').forEach(function (wrap) {
             io.observe(wrap);
         });
     });
 }
-function getBase64ByURL(imgUrl) {
-    return new Promise(resolve => {
-        window.URL = window.URL || window.webkitURL;
-        var xhr = new XMLHttpRequest();
-        xhr.open("get", imgUrl, true);
-        xhr.responseType = "blob";
-        xhr.onload = function () {
-            if (this.status == 200) {
-                var blob = this.response;
-                let oFileReader = new FileReader();
-                oFileReader.onloadend = function (e) {
-                    resolve({ blob, base64: e.target.result })
-                };
-                oFileReader.readAsDataURL(blob);
-            }
-        };
-        xhr.send();
-    });
-};
-
 
 function iloader(imgUrl) {
-    return new Promise(resolve => {
-        let image = new Image()
+    var id = imgUrl.match(/\d+/)
+
+    return new Promise((resolve, reject) => {
         window.URL = window.URL || window.webkitURL;
-        var xhr = new XMLHttpRequest();
+        let xhr = new XMLHttpRequest();
         xhr.open("get", imgUrl, true);
         xhr.responseType = "blob";
         xhr.onload = function () {
+
             if (this.status == 200) {
+
                 var blob = this.response;
                 let oFileReader = new FileReader();
                 oFileReader.onloadend = function (e) {
@@ -108,10 +98,106 @@ function iloader(imgUrl) {
                 };
                 oFileReader.readAsDataURL(blob);
             }
+
         };
         xhr.send();
+        xhr.addEventListener("progress", updateProgress);
+        function updateProgress(event) {
+            if (event.lengthComputable) {
+                let progress = event.loaded / event.total * 100;
+                //console.log(`${id}加载进度: ${progress}%`)
+                let progress_div = document.querySelector(`.lazyImg-wrap[data-src="${imgUrl}"] > div >div> div`)
+                
+                progress_div.style = `width:${progress}%`
+                
+                if (progress> 80) {
+                    progress_div.style = `width:100%`
+                }
+
+
+            }
+        }
+
+        xhr.onreadystatechange = function () {
+            //console.log(xhr.readyState)
+
+            if (this.readyState === this.DONE) {
+                if (this.responseURL == '') {
+                    console.log("301失败")
+                    this.abort()
+                    let rexhr = new XMLHttpRequest();
+                    rexhr.open("get", `https://pixiv.re/${id}-1.png`, true);
+                    rexhr.responseType = "blob";
+                    rexhr.addEventListener("progress", updateProgress);
+                    rexhr.onload = function () {
+                        
+                        if (this.status == 200) {
+
+                            var blob = this.response;
+                            let oFileReader = new FileReader();
+                            oFileReader.onloadend = function (e) {
+                                resolve({ blob, base64: e.target.result })
+                            };
+                            oFileReader.readAsDataURL(blob);
+                        }
+
+                    };
+                    rexhr.send();
+                }
+
+            }
+        }
+
+    }).catch((err) => {
+        console.log('aaa' + err)
     });
+
+
 }
+
+/*
+function jsonp (url, callback) {
+    let scriptTag = document.createElement('script')
+    
+    scriptTag.src = scriptTag.src = url + '?' +  'fn=' +callback
+    document.querySelector('head').appendChild(scriptTag)
+}
+function myCallback(){
+    console.log("succ")
+}
+jsonp ('https://pixiv.js.org/ajax/illust/106221844/pages', myCallback())
+
+
+function fetchJSON_comment98($str) {
+    console.log('我是回调函数-我先执行');
+    console.log($str);
+}
+
+
+
+function test_jsonp() {
+    $.ajax({
+        type: 'get',
+        url: 'https://pixiv.js.org/ajax/illust/106221844/pages',
+        dataType: "jsonp",
+        jsonpCallback: 'fetchJSON_comment98',
+        success: function (result) {
+            console.log(result + '先执行完回调函数，我在执行');
+
+        },
+        error: function (result) {
+            console.log(result)
+            console.log('失败');
+        }
+    })
+}
+test_jsonp()
+
+
+*/
+
+
+
 function downloadButtonClick(url) {
 
     let imgselect = document.querySelector(`#illustcontainer>.col>div>img[data-src="${url}"]`)
