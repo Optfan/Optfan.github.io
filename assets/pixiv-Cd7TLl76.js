8ad653b07c03d77e657a25e20e3af5e5ad0a723f
cpufan <554950835@qq.com>
2024-12-17 23:02:23 +0800

    更换域名

--- assets/pixiv-Cd7TLl76.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./axios-CV-JwR63.js";import{W as l}from"./index-Bw1ld_LV.js";const c=async t=>{let r,a=2;for(;;)try{await o.head(`${l.pixiv_proxy_baseUrl}/${t}-${a}.png`),r=a,a=2*a}catch{if(a==2)return 1;break}let e,i;for(;r<=a;){e=Math.floor((r+a)/2);try{await o.head(`${l.pixiv_proxy_baseUrl}/${t}-${e}.png`),r=e+1}catch{i=e,a=e-1}}return i-1};export{c as h};
